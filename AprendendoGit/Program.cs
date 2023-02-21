ef1c02802b32644e16ceec640a1b48cc7eb089b1
Fernando Souza <juniorlimafl@gmail.com>
2023-02-21 12:21:24 -0300

    commit initial

---
+++ AprendendoGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AprendendoGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendendo Git e GitHub controlando versionamento");
+        }
+    }
+}
